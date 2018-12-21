8471d8817270c1dfc67ee1931aa48b065772d39c
David Knight <DKnight96@sbcglobal.net>
2018-12-20 16:29:17 -0800

    Changed initialization and documentation for ledTask
    Changed initialization and documentation for ledTask

--- far-mars-onboard-firmware/src/led.c
+++ far-mars-onboard-firmware/src/led.c
@@ -8,7 +8,9 @@
 #include <asf.h>
 #include "led.h"
 
+//! @brief Red LED
 #define LED0 PIN_PA12
+//! @brief Green LED
 #define LED1 PIN_PA13
 
 /**
@@ -17,18 +19,23 @@
  * @return		none.
  */
 void ledTask(void *pvParameters) {
+	/* Initialization */
 	struct port_config config_port_pin;
 	port_get_config_defaults(&config_port_pin);
 	config_port_pin.direction = PORT_PIN_DIR_OUTPUT;
 	port_pin_set_config(LED0, &config_port_pin);
 	port_pin_set_config(LED1, &config_port_pin);
+	
+	port_pin_set_output_level(LED0, 1);
+	port_pin_set_output_level(LED1, 1);
 
+	/* Task code */
 	while(1) {
 		port_pin_set_output_level(LED0, 1);
-		port_pin_set_output_level(LED0, 0);
+		port_pin_set_output_level(LED1, 0);
 		vTaskDelay(pdMS_TO_TICKS(500));
 		port_pin_set_output_level(LED0, 0);
-		port_pin_set_output_level(LED0, 1);
+		port_pin_set_output_level(LED1, 1);
 		vTaskDelay(pdMS_TO_TICKS(500));
 	}
 }
