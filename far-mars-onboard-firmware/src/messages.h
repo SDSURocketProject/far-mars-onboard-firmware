aa0baa5f9ce22a8454a302cb4ba9c9d206cfa554
David Knight <DKnight96@sbcglobal.net>
2019-01-06 21:17:57 -0800

    Added pressures to messages.h, renamed structs in sensorMessage

--- far-mars-onboard-firmware/src/messages.h
+++ far-mars-onboard-firmware/src/messages.h
@@ -30,7 +30,10 @@ enum sensorMessageIDs {
 	cpuTemperatureRawDataID,
 	cpuTemperatureCelciusDataID,
 	cpuTemperatureFahrenheitDataID,
-	cpuTemperatureKelvinDataID
+	cpuTemperatureKelvinDataID,
+	pressureRawDataID,
+	pressurePSIADataID,
+	pressurePSIGDataID
 };
 
 //! @brief Struct containing raw CPU temperature data
@@ -43,6 +46,16 @@ struct cpuTemperatureFloatData {
 	float temperature;
 };
 
+//! @brief Struct containing pressure sensor data
+struct pressureData {
+	uint16_t P1, P2, P3;
+};
+
+//! @brief Struct containing pressure sensor data
+struct pressureFloatData {
+	float P1, P2, P3;
+};
+
 /**
  * @brief Struct that contains all messages that can be sent, must be packed for communication.
  */
@@ -52,25 +65,29 @@ struct __attribute__((packed, aligned(1))) sensorMessage {
 
 	// Data
 	union {
-		struct bno055_accel_t              accelerationRawData;
-		struct bno055_gyro_t               gyroscopeRawData;
-		struct bno055_euler_t              eulerRawData;
-		struct bno055_quaternion_t         quaternionRawData;
-		struct bno055_linear_accel_t       linearAccelerationRawData;
-		struct bno055_gravity_t            gravityRawData;
+		struct bno055_accel_t              accelerationRaw;
+		struct bno055_gyro_t               gyroscopeRaw;
+		struct bno055_euler_t              eulerRaw;
+		struct bno055_quaternion_t         quaternionRaw;
+		struct bno055_linear_accel_t       linearAccelerationRaw;
+		struct bno055_gravity_t            gravityRaw;
 
-		struct bno055_accel_float_t        accelerationMpssData;       // Meters per second squared
-		struct bno055_gyro_float_t         gyroscopeRpsData;           // Degrees per second
-		struct bno055_gyro_float_t         gyroscopeDpsData;           // Radians per second
-		struct bno055_euler_float_t        eulerRadianData;
-		struct bno055_euler_float_t        eulerDegreeData; 
-		struct bno055_linear_accel_float_t linearAccelerationMpssData; // Meters per second squared
-		struct bno055_gravity_float_t      gravityMpssData;            // Meters per second squared
+		struct bno055_accel_float_t        accelerationMpss;       // Meters per second squared
+		struct bno055_gyro_float_t         gyroscopeRps;           // Degrees per second
+		struct bno055_gyro_float_t         gyroscopeDps;           // Radians per second
+		struct bno055_euler_float_t        eulerRadian;
+		struct bno055_euler_float_t        eulerDegree; 
+		struct bno055_linear_accel_float_t linearAccelerationMpss; // Meters per second squared
+		struct bno055_gravity_float_t      gravityMpss;            // Meters per second squared
 
-		struct cpuTemperatureData          cpuTemperatureRawData;
-		struct cpuTemperatureFloatData     cpuTemperatureCelciusData;
-		struct cpuTemperatureFloatData     cpuTemperatureFahrenheitData;
-		struct cpuTemperatureFloatData     cpuTemperatureKelvinData;
+		struct cpuTemperatureData          cpuTemperatureRaw;
+		struct cpuTemperatureFloatData     cpuTemperatureCelcius;
+		struct cpuTemperatureFloatData     cpuTemperatureFahrenheit;
+		struct cpuTemperatureFloatData     cpuTemperatureKelvin;
+
+		struct pressureData                pressureRaw;
+		struct pressureFloatData           pressurePSIA; // Absolute pressure
+		struct pressureFloatData           pressurePSIG; // Gauge pressure
 	};
 };
 
