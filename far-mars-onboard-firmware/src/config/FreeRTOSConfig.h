c59a1b77789e5e7aba52e7b1248bba787ad3e0da
David Knight <DKnight96@sbcglobal.net>
2019-02-19 19:06:34 -0800

    Created release version of configASSERT

--- far-mars-onboard-firmware/src/config/FreeRTOSConfig.h
+++ far-mars-onboard-firmware/src/config/FreeRTOSConfig.h
@@ -138,7 +138,11 @@ See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
 
 /* Normal assert() semantics without relying on the provision of an assert.h
 header file. */
+#ifdef DEBUG
 #define configASSERT( x ) if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); for( ;; ); }
+#else
+#define configASSERT( x )
+#endif
 
 /* Definitions that map the FreeRTOS port interrupt handlers to their CMSIS
 standard names.Based on the architecture  choosen, the below definitons may be needed*/
