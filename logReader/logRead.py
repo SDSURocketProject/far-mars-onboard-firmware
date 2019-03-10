ae59623928f870fff9a9c8a0d87866fb4810c112
David Knight <DKnight96@sbcglobal.net>
2019-03-09 18:57:28 -0800

    Added check to logReader to determine if data was lost

--- logReader/logRead.py
+++ logReader/logRead.py
@@ -6,6 +6,15 @@ allMessages = sm.unpackMessages("data.log")
 
 allMessages.sort(key=lambda tup: tup[1]) # Sorting by timestamp
 
+# Check for lost data
+last = allMessages[1][1]
+for message in allMessages:
+    if (message[1] - last > 10):
+        print(f"{last}, {message[1]}, {message[1] - last}")
+    
+    last = message[1]
+    
+
 #allMessages = convertMessages(allMessages, sm.accelerationRawDataID, sm.accelerationMpssDataID)
 #allMessages = convertMessages(allMessages, sm.gyroscopeRawDataID, sm.gyroscopeRpsDataID)
 #allMessages = convertMessages(allMessages, sm.pressureRawDataID, sm.pressurePSIGDataID)
@@ -14,5 +23,5 @@ allMessages.sort(key=lambda tup: tup[1]) # Sorting by timestamp
 #sm.writeLog(allMessages, "formattedData.log")
 #sm.writeCSV(allMessages, sm.accelerationMpssDataID, "Acceleration.csv")
 #sm.writeCSV(allMessages, sm.gyroscopeRpsDataID, "Gyroscope.csv")
-sm.writeCSV(allMessages, sm.pressurePSIGDataID, "Pressure.csv")
+#sm.writeCSV(allMessages, sm.pressurePSIGDataID, "Pressure.csv")
 #sm.writeCSV(allMessages, sm.cpuTemperatureCelciusDataID, "CPU_Temperature.csv")
