4b321ec1a0e3c646d66dd4c7eaab82cb99e02135
David Knight <DKnight96@sbcglobal.net>
2019-04-02 16:34:16 -0700

    Configured hall effect sensor inputs to use pull up resistors

--- far-mars-onboard-firmware/src/hall.c
+++ far-mars-onboard-firmware/src/hall.c
@@ -23,6 +23,7 @@ int hallInit(void) {
 	struct port_config config_port_pin;
 	port_get_config_defaults(&config_port_pin);
 	config_port_pin.direction = PORT_PIN_DIR_INPUT;
+	config_port_pin.input_pull = PORT_PIN_PULL_UP;
 	port_pin_set_config(METHANE_VENT_VALVE_PIN, &config_port_pin);
 	port_pin_set_config(LOX_VENT_VALVE_PIN, &config_port_pin);
 	
